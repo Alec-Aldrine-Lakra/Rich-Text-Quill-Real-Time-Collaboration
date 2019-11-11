d911b0b525240fbe72785169c71862d5293dcb61
Alec Aldrine Lakra <lakraalec123@gmail.com>
2019-11-11 18:55:43 +0530

    Quill table and image resize

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from "@angular/core";
+
+interface Quill{
+  getModule(moduleName: string);
+}
+
+interface BetterTableModule {
+  insertTable(rows: number, columns: number): void;
+}
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
+
 export class AppComponent {
-  title = 'table';
+  public quill: Quill;
+  private get tableModule(): BetterTableModule {
+    return this.quill.getModule("better-table");
+  }
+
+  public editorCreated(event:Quill): void {
+    this.quill = event;
+    // Example on how to add new table to editor
+    this.addNewtable();
+  }
+
+  private addNewtable(): void {
+   this.tableModule.insertTable(3, 3);
+ }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,39 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { QuillConfig, QuillModule } from "ngx-quill";
+import * as Quill from "quill";
+import QuillBetterTable from "quill-better-table";
+import { AppComponent } from "./app.component";
+import ImageResize  from 'quill-image-resize';
+Quill.register({'modules/imageResize' : ImageResize},true);
+Quill.register({'modules/better-table' : QuillBetterTable},true);
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-
+const quillConfig: QuillConfig = {
+  modules: {
+    imageResize: {},
+    table: false, // disable table module
+    "better-table": {
+      operationMenu: {
+        items: {
+          unmergeCells: {
+            text: "Another unmerge cells name"
+          }
+        },
+        color: {
+          colors: ["#fff", "red", "rgb(0, 0, 0)"], // colors in operationMenu
+          text: "Background Colors" // subtitle
+        }
+      }
+    },
+    keyboard: {
+      bindings: QuillBetterTable.keyboardBindings
+    }
+  }
+};
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, QuillModule.forRoot(quillConfig)],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule  { }
